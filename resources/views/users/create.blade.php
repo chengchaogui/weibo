b38b5703a9b3ec4cc9da1ce6ed2cdde42d9262da
chengchaogui <393440223@qq.com>
2019-03-15 08:52:08 +0000

    中文的错误提示信息

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -7,8 +7,12 @@
                 <h5>注册</h5>
             </div>
             <div class="card-body">
+
+                @include('shared._errors')
+
                 <form method="POST" action="{{ route('users.store') }}">
                     {{ csrf_field() }}
+
                     <div class="form-group">
                         <label for="name">名称：</label>
                         <input type="text" name="name" class="form-control" value="{{ old('name') }}">
@@ -26,8 +30,7 @@
 
                     <div class="form-group">
                         <label for="password_confirmation">确认密码：</label>
-                        <input type="password" name="password_confirmation" class="form-control"
-                               value="{{ old('password_confirmation') }}">
+                        <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
                     </div>
 
                     <button type="submit" class="btn btn-primary">注册</button>
